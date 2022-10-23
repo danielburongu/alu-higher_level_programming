cccb60c7b206ce50e5773e1895daad54120f8b3b
daniel burongu <burongudaniel@gmail.com>
2022-10-24 01:14:29 +0200

    my first commit

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number=random.randint(-10,10)
+if number==0:
+    print(f"the {number} is zero")
+elif number >0:
+    print(f"the {number} is positive")
+else:
+    print(f"the {number} is negative")
+    
+
