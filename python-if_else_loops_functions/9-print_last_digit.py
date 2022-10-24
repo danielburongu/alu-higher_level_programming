aaf1c9e5d676443a393d6d4e7e32c5f46dcade70
daniel burongu <burongudaniel@gmail.com>
2022-10-24 12:20:21 +0200

    print string in uppercase

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for char in str:
         if 123 > ord(char) > 96:
             char = chr(ord(char) - 32)
-        print('{}'.format(char), end="")
-    print('')
+    print('{}'.format(char), end="")
+print('')
