768942cce9c92be7ff0f38b5c32b01a40748bcaa
daniel burongu <burongudaniel@gmail.com>
2022-11-02 09:18:18 +0200

    added my first file

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    numbers= 1, 2, 3, 4, 5
+    numbers= 1 , 2, 3, 4, 5
     for numbers in my_list:
         print('{:d}'.format(numbers))
