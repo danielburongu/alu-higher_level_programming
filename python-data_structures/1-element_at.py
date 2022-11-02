8df6ceb3bd45a18ebdb0de8f80b768cd475f7e90
daniel burongu <burongudaniel@gmail.com>
2022-11-02 10:42:44 +0200

    added my second file

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    element_at = __import__('1-element_at').element_at
-    my_list = [1, 2, 3, 4, 5]
-idx = 3
-print("Element at index {:d} is {1}".format(idx, element_at(1,2,3,4,5, idx)))
+    if idx < 0:
+        return None
+        if idx >= len(my_list):
+            return None
+            return my_list[idx]
