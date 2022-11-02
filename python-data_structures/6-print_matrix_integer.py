7e21ca9cb7bca0d19d4aecbd01bfe94b6e24bad4
daniel burongu <burongudaniel@gmail.com>
2022-11-02 15:41:42 +0200

    added my matrix file

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     def no_c(my_string):
         my_string = my_string.translate({ord("c"): None})
-    my_string = my_string.translate({ord("C"): None})
-    return my_string
+        my_string = my_string.translate({ord("C"): None})
+        return my_string
