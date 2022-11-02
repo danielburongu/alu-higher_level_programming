a3e88029664cd0c5cbc9cb68cdeb1eff6e9508a6
daniel burongu <burongudaniel@gmail.com>
2022-11-02 10:57:46 +0200

    added my second file

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
     elif idx >= len(my_list):
-         return None
+        return None
     else:
-            return my_list[idx]
+        return my_list[idx]
