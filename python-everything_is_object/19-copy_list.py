9493f25c91556accaa1c9905aa197dcd369f16ff
daniel burongu <burongudaniel@gmail.com>
2022-11-15 15:57:56 +0200

    updates commit

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def copy_list(l):
-    my_list = [1, 2, 3]
-    print(my_list)
-    new_list = copy_list(my_list)
-    print(my_list)
-    print(new_list)
-    print(new_list == my_list)
-    print(new_list is my_list)
+    return l.copy()
