eac027d404c44400b7c5e92043c64cbd0cc5d28d
daniel burongu <burongudaniel@gmail.com>
2022-11-16 10:23:39 +0200

    updates

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-'''a script that returns True if the object is
+''' a function that returns True if the object is
 an instance of a class that inherited (directly or indirectly)'''
-
 
 
 def inherits_from(obj, a_class):
