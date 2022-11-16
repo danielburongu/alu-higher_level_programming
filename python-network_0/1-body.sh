3e17db05b6b32f04b94c959305201ce5092e3488
daniel burongu <burongudaniel@gmail.com>
2022-11-17 00:16:50 +0200

    all files updated

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-#Bash script that sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -X GET -L 
+# a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -s "$1" -X GET -L
