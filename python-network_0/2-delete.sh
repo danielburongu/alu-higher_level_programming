94ff651ebf6c8de244989e6b995581e8a85c7483
daniel burongu <burongudaniel@gmail.com>
2022-11-17 11:35:42 +0200

    new line

--- python-network_0/2-delete.sh
+++ python-network_0/2-delete.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #Sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -s "$1" -X DELETE                                                                      
+curl -s "$1" -X DELETE
+                                                                   
