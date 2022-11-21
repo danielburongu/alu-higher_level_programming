25bf8a97bd1b6e4f2bf292b86ff6e76168cd5a74
daniel burongu <burongudaniel@gmail.com>
2022-11-22 01:35:44 +0200

    added a file

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import urllib.request
+"""
+Script that fetches https://alu-intranet.hbtn.io/status
+"""
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as reply:
+        body = reply.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode(encoding="utf -8")))
