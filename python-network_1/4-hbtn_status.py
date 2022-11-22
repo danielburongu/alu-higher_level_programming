88b398a81936e86395820372db6fc8bb455256b0
daniel burongu <burongudaniel@gmail.com>
2022-11-22 10:23:28 +0200

    my changes

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Fetch a url"""
+"""Fetch url requests"""
 
 import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status")
+    reply = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(reply.text)))
+    print("\t- content: {}".format(reply.text))
