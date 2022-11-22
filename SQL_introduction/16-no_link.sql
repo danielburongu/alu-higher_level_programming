0645ef2e02ef78af68a050b4b50a3caca3370e04
daniel burongu <burongudaniel@gmail.com>
2022-11-22 17:07:44 +0200

    my file changes

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,4 @@
--- list all record in the database table 
+-- showing all the record in the database table
+-- in MYSQL server 
 SELECT score, name FROM second_table
 WHERE name IS NOT NULL ORDER BY score DESC;
