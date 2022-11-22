9edb752c84bfaa20912463921229e21f9b7aa58e
daniel burongu <burongudaniel@gmail.com>
2022-11-22 17:00:48 +0200

    my file changes

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
---a script that lists all records of the table second_table
+-- a script that lists all records of the table second_table
 -- of the database in MYSQL sever
 SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
