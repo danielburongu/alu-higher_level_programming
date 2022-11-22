480672eed391809f2e6afecf2dc60c90335ab87b
daniel burongu <burongudaniel@gmail.com>
2022-11-22 16:58:50 +0200

    my file changes

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
--- A script that lists all the records with the same score in the table
+--a script that lists all records of the table second_table
 -- of the database in MYSQL sever
 SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
