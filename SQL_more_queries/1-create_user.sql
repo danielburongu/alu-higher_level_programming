b22cadaded198d46db9525d6337adf6d2d07e8f5
daniel burongu <burongudaniel@gmail.com>
2022-11-24 00:36:25 +0200

    my updates!

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- a script that creates the MySQL server user 
+--  creates the MySQL server user 
 --  Root user
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PERMISSIONS ON *.* TO user_0d_1@localhost
