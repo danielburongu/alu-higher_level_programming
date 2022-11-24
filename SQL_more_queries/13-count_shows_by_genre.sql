aae553f4e46f8927d1da481ba41ba1b575d2c74f
daniel burongu <burongudaniel@gmail.com>
2022-11-24 13:47:42 +0200

    updates!

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- a script that lists all genres 
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_show_genres
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
-ORDER BY number_shows DESC;
+ORDER BY number_of_shows DESC;
