6c0cb9e73deaecf96e383ff0488ebfb5929a38fa
danielburongu <d.burongu@alustudent.com>
2023-01-30 23:49:50 +0300

    prints 3 lines

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // a script that prints 3 lines
-let myVar = 'C is fun\n' +
+const myVar = 'C is fun\n' +
 'Python is cool\n' +
 'JavaScript is amazing' 
 console.log(myVar);
---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,2 @@
+#!/usr/bin/node
+// a script that prints a message depending of the number of arguments passed
