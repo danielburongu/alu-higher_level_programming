bcafcea4158f13d13462c4ba9babbd9cc967a945
danielburongu <d.burongu@alustudent.com>
2023-01-31 23:22:18 +0300

    files update

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 // a script that replaces the value 12 with 89
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.value = 89;
-  console.log(myObject);
-  
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
