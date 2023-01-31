616085d06ec3ac47675ca20671f312c7b704e539
danielburongu <d.burongu@alustudent.com>
2023-01-31 10:14:32 +0300

    concat arguments

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 // a script that prints two arguments passed to it, in the following format: “ is ”
-console.log(process.argv[2] + ' is '  + process.argv[3]);
+console.log(process.argv[2]+ ' is '  + process.argv[3]);
