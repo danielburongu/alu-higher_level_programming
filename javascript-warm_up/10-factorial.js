47ec30a600d884bcaf1b390d372f3156fa09eefd
danielburongu <d.burongu@alustudent.com>
2023-01-31 20:51:39 +0300

    files update

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
 // script that computes and prints a factorial
-function factorial (n) {
-  if (n === 0) return 1;
-  return n * factorial(n - 1);
+function factorial(num) {
+    if (isNaN(num) || num < 0) {
+      return 1;
+    } else if (num === 0) {
+      return 1;
+    } else {
+      return num * factorial(num - 1);
+    }
 }
-console.log(factorial(parseInt(process.argv[2])) || 1);
+  const arg = parseInt(process.argv[2]);
+  console.log(factorial(arg));
---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+const str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const str of str) {
+  console.log(str);
+}
