7262e9baa744c3ceee8d69af00d38bf8405adce3
danielburongu <d.burongu@alustudent.com>
2023-01-31 13:42:25 +0300

    multi_languages loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/bin/bash/node
+// loop to language
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+languages.forEach(function(language) {
+  console.log(language);
+});
