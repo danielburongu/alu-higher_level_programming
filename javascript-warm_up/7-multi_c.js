5f7041838b2b96bbab339c58d5ca7a69332883d1
danielburongu <d.burongu@alustudent.com>
2023-01-31 19:26:03 +0300

    files update

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-// a script that prints x times “C is fun”
+// a script that prints x times 'C is fun'
 let x = parseInt(process.argv[2]);
 if (isNaN(x)) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < x; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 }
