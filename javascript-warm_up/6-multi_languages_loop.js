c214eae7ac9041063fdae42db91802b27db8609d
danielburongu <d.burongu@alustudent.com>
2023-01-31 23:01:15 +0300

    files update

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < languages.length; i++) {
   console.log(languages[i]);
