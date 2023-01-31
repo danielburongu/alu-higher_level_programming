a69864aaa7942b7037fd83c77b00d1943ee5993a
danielburongu <d.burongu@alustudent.com>
2023-01-31 19:30:49 +0300

    files update

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const x = parseInt(process.argv[2]);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (const i = 0; i < x; i++) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
