0860a580c15566a32491f3f5efb6d1a402087fa6
danielburongu <d.burongu@alustudent.com>
2023-01-31 09:43:43 +0300

    arguments

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,2 +1,4 @@
 #!/usr/bin/node
-//
+//process.argv[2] gives the value of the first argument passed to the script.
+//The code uses the logical OR operator (||) to check if there is a second argument 
+console.log(process.argv[2] || "No argument");
