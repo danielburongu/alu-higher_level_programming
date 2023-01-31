6d43f20057379e01aff385f6833bb9b1fadc2b95
danielburongu <d.burongu@alustudent.com>
2023-01-31 22:34:06 +0300

    files update

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // script that computes and prints a factorial
-function factorial(num) {
+function factorial (num) {
   if (isNaN(num) || num < 0) {
     return 1;
-   } else if (num === 0) {
+  } else if (num === 0) {
     return 1;
-   } else {
+  } else {
     return num * factorial(num - 1);
-   }
+  }
 }
 const arg = parseInt(process.argv[2]);
 console.log(factorial(arg));
