f6dbcc55b9e8ad93e1426ce4202eaaad83cb72f0
danielburongu <d.burongu@alustudent.com>
2023-01-31 12:50:16 +0300

    integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 // isNaN(Number(process.argv[2])) checks if the conversion failed (i.e., if the argument is not a number).
 const arg = Number(process.argv[2]);
 if (isNaN(arg)) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
-  console.log("My number: " + arg);
+  console.log('My number: ' + arg);
 }
