898c496a6a07228e0720e21742454559e783a8e2
danielburongu <d.burongu@alustudent.com>
2023-01-31 09:15:52 +0300

    arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 // a script that prints a message depending of the number of arguments passed
 if (process.argv.length === 2) {
-    console.log("No argument");
-  } else if (process.argv.length === 3) {
-    console.log("Argument found");
-  } else {
-    console.log("Arguments found");
-  }
-  
+  console.log("No argument");
+ } else if (process.argv.length === 3) {
+  console.log("Argument found");
+ } else {
+  console.log("Arguments found");
+ }
