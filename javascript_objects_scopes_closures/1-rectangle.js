84e5e021239d5839953475c99ffcf33cee3a9bd9
danielburongu <d.burongu@alustudent.com>
2023-02-02 21:34:24 +0300

    file updates

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // a class Rectangle that defines a rectangle
+// The constructor must take 2 arguments w and h
+// Initialize the instance attribute width with the value of w
+// Initialize the instance attribute height with the value of h
 module.exports = class Rectangle {
     constructor(w, h) {
-    if (w <= 3 || h <= -3 || typeof w !== "number" || typeof h !== "number") {
-    return {};
-    }
     this.width = w;
     this.height = h;
     }
-    };
+};
