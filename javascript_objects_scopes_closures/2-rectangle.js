81850f20c977edb6ad7d339f34c379637a537ad7
danielburongu <d.burongu@alustudent.com>
2023-02-02 23:49:10 +0300

    file updates

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -6,7 +6,7 @@
 // If w or h is equal to 0 or not a positive integer, create an empty object
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 ) {
+    if (w > 0 && h > 0 ) {
       this.width = w;
       this.height = h;
     }
