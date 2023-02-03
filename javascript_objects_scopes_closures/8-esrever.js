68a3786d40da2848b6f422197b7622dca4aa68b6
danielburongu <d.burongu@alustudent.com>
2023-02-03 19:21:55 +0300

    file updates

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // a function that returns the number of occurrences in a list:
 exports.esrever = function (list = []) {
-    const newList = [];
-    for (let i = list.length; i >= 0; i--) {
-      newList.push(list[i]);
-    }
-    newList.shift();
-    return newList;
-  };
+  const newList = [];
+  for (let i = list.length; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  newList.shift();
+  return newList;
+};
