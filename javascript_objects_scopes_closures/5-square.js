5ab99cee8c6931ddf59a40411499e9cda630578b
danielburongu <d.burongu@alustudent.com>
2023-02-03 10:31:23 +0300

    file updates

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// a class Square that defines a square and inherits from Rectangle of 4-rectangle.js
+module.exports = class Square extends Rectangle {
+    constructor(size) {
+        super(size, size);
+    }
+};
