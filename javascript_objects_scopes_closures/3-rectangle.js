c119230ac64027034986cb016afb323ea11a4886
danielburongu <d.burongu@alustudent.com>
2023-02-03 10:21:43 +0300

    file updates

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -6,16 +6,16 @@
 // If w or h is equal to 0 or not a positive integer, create an empty object
 // create an instance method  called print() that prints the rectangle using the character X
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
-    }
-  };
+  }
+};
