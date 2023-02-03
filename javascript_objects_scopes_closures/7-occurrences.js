3a88ff6a1cb4090f9003f974d164f290aa11a7bd
danielburongu <d.burongu@alustudent.com>
2023-02-03 20:38:22 +0300

    file updates

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// a function that returns the number of occurrences in a list:
-exports.nbOccurrences = function (list, searchElement) {
+// a function that returns the number of occurrences in a list
+exports.nbOccurences = function (list, searchElement) {
     return list.filter((n) => n === searchElement).length;
   };
