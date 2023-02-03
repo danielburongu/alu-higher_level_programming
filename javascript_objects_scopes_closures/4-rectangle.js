3e1825321624ea7de3bf56235e454c40856c3587
danielburongu <d.burongu@alustudent.com>
2023-02-03 22:47:19 +0300

    file updates

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -2,18 +2,18 @@
 // a class Rectangle that defines a rectangle
 // must use the class notation for defining your class
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
   
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
+  }
   
     rotate () {
       const tempWidth = this.width;
