5cbd73f9621c1ab50c5482ba85c0a28f8174d8de
danielburongu <d.burongu@alustudent.com>
2023-02-03 20:17:16 +0300

    file updates

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//  a function that prints the number of arguments already printed and the new argument value. 
+// a function that prints the number of arguments already printed and the new argument value. 
 exports.logMe = function (item) {
   if (typeof this.count === 'undefined') this.count = 0;
   console.log(this.count + ': ' + item);
