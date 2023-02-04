003da2bfdace7a672944741f151d34a0ad0a5978
danielburongu <d.burongu@alustudent.com>
2023-02-04 23:00:28 +0300

    file updates

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -3,13 +3,13 @@
 const req = require('request');
 
 const getCompletedTasks = (data, userId) =>
-  data.filter(({userId: id, completed}) => id === userId && completed).length;
+  data.filter(({ userId: id, completed }) => id === userId && completed).length;
 
 req(process.argv[2], (err, res, body) => {
   if (err) throw err;
   console.log(
     JSON.parse(body)
-      .reduce((res, {userId}) => {
+      .reduce((res, { userId }) => {
         if (!res[userId]) {
           const completed = getCompletedTasks(JSON.parse(body), userId);
           if (completed) res[userId] = completed;
