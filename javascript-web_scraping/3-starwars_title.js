e7d877e414827ad61a7e150953bd22f0bef12bc2
danielburongu <d.burongu@alustudent.com>
2023-02-04 22:07:37 +0300

    file updates

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 // a script that prints the title of a Star Wars movie where the episode number matches a given integer.
-const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const req = require('request');
 
-request(url, (err, response, body) => {
-  if (err) {
-    console.error(err);
-  } else {
-    const movie = JSON.parse(body);
-    console.log(` ${movie.title}`);
-  }
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+req.get(url, (err, res) => {
+  err ? console.log(err) : console.log(JSON.parse(res.body).title);
 });
