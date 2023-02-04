65aeeadcd4c8737e41a2d71feef3507b5d0a3d51
danielburongu <d.burongu@alustudent.com>
2023-02-04 22:28:34 +0300

    file updates

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,24 @@
 #!/usr/bin/node
 // a script that computes the number of tasks completed by user id.
 const request = require('request');
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-request(apiUrl, (error, response, body) => {
-  if (error) return console.error(error);
-  
-  const tasks = JSON.parse(body).filter(task => task.completed);
-  const userTasks = {};
-  
-  tasks.forEach(task => {
-    if (!userTasks[task.userId]) userTasks[task.userId] = 0;
-    userTasks[task.userId]++;
-  });
-  
-  const filteredTasks = {};
-  for (const userId in userTasks) {
-    if (userId === '1' || userId === '2') filteredTasks[userId] = userTasks[userId];
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const tasks = JSON.parse(body);
+    const completedTasksByUser = {};
+    tasks.forEach(task => {
+      if (task.completed) {
+        if (!completedTasksByUser[task.userId]) {
+          completedTasksByUser[task.userId] = 1;
+        } else {
+          completedTasksByUser[task.userId]++;
+        }
+      }
+    });
+    console.log(completedTasksByUser);
   }
-  
-  console.log({'1': 3, '2': 3});
 });
