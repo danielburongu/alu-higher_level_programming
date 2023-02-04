07041d9f0851d911ccacc8e17277142ed11aae6e
danielburongu <d.burongu@alustudent.com>
2023-02-04 16:28:53 +0300

    file updates

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// a script that writes a string to a file.
+const fs = require('fs');
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, 'utf-8', (error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('file successfully saved!');
+  }
+});
