18b49b4529d77ff44f265f511a2f46534743fc1e
danielburongu <d.burongu@alustudent.com>
2023-02-04 21:50:35 +0300

    file updates

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that display the status code of a GET request.
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
